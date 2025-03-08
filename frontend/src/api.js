dd5a923501809afdf2fd5ee5fbf719996a7ae514
loghashrichandiramohan <loghashri1506@gmail.com>
2025-03-08 23:27:49 +0530

    server.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://dishgenie-project.onrender.com , // Your backend URL
+  baseURL: "https://dishgenie-project.onrender.com" , // Your backend URL
 });
 
 // Attach token to requests (for authenticated routes)
